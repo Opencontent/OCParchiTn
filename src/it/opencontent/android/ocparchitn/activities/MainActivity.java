089ae0cfcbd5cca1ecc4f9d1fe0004406d1f052a
Marco Albarelli <info@marcoalbarelli.eu>
2012-10-07 20:38:07 +0200

    Implementata la lettura dei mifareUltralightC in maniera abbastanza cruda Estrazione dei campi tramite RegExp Modificato il layout per contenere i campi estratti

--- src/it/opencontent/android/ocparchitn/activities/MainActivity.java
+++ src/it/opencontent/android/ocparchitn/activities/MainActivity.java
@@ -2,7 +2,12 @@ package it.opencontent.android.ocparchitn.activities;
 
 import it.opencontent.android.ocparchitn.R;
 import it.opencontent.android.ocparchitn.services.SOAPService;
-import android.app.Activity;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -10,13 +15,14 @@ import android.content.IntentFilter.MalformedMimeTypeException;
 import android.graphics.Bitmap;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.MifareUltralight;
 import android.nfc.tech.NfcF;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends BaseActivity {
 
@@ -26,6 +32,8 @@ public class MainActivity extends BaseActivity {
 	private static IntentFilter[] ifa;
 	private static String[][] techListsArray;
 	private static Bitmap mImageBitmap;
+	private Pattern p = Pattern.compile("ID:\\d+");
+    private Pattern p2 = Pattern.compile("SER:[\\p{L}0-9-_ :]+");
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -46,7 +54,7 @@ public class MainActivity extends BaseActivity {
 			throw new RuntimeException("fail", e);
 		}
 		ifa = new IntentFilter[] { ndef, };
-		techListsArray = new String[][] { new String[] { NfcF.class.getName() } };
+		techListsArray = new String[][] { new String[] { NfcF.class.getName(), MifareUltralight.class.getName(), MifareClassic.class.getName() } };
 
 		getTestSOAPRequest();
 		
@@ -64,6 +72,7 @@ public class MainActivity extends BaseActivity {
 	public void onResume() {
 		super.onResume();
 		nfca.enableForegroundDispatch(this, pi, ifa, techListsArray);
+		
 		if (mImageBitmap != null) {
 			ImageView mImageView = (ImageView) findViewById(R.id.snapshot);
 			mImageView.setImageBitmap(mImageBitmap);
@@ -72,7 +81,53 @@ public class MainActivity extends BaseActivity {
 
 	@Override
 	public void onNewIntent(Intent intent) {
-		Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+		String res = "";
+		MifareUltralight mifare = MifareUltralight.get((Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG));
+		byte[] payload;
+		byte[] payload2;
+		byte[] payload3;
+		
+        try {
+            mifare.connect();
+            payload = mifare.readPages(4);
+            payload2 = mifare.readPages(8);
+            payload3 = mifare.readPages(12);
+ 
+            //return new String(payload, Charset.forName("US-ASCII"));
+            res = new String(payload, Charset.forName("UTF-8"));
+            res += new String(payload2, Charset.forName("UTF-8"));
+            res += new String(payload3, Charset.forName("UTF-8"));
+        } catch (IOException e) {
+            Log.e(TAG, "IOException while writing MifareUltralight message...", e);
+        } finally {
+            if (mifare != null) {
+               try {
+                   mifare.close();
+               }
+               catch (IOException e) {
+                   Log.e(TAG, "Error closing tag...", e);
+               }
+            }
+        }
+        
+        
+        Matcher m = p.matcher(res);
+        Matcher m2 = p2.matcher(res);
+        String name = "";
+        String ser = "";
+        while (m.find()) { // Find each match in turn; String can't do this.
+            name = m.group(); // Access a submatch group; String can't do this.
+        }
+        while (m2.find()) { // Find each match in turn; String can't do this.
+        	ser = m2.group(); // Access a submatch group; String can't do this.
+        }
+        
+        TextView giocoId = (TextView) findViewById(R.id.display_gioco_id);
+        giocoId.setText(name);
+        TextView giocoSeriale = (TextView) findViewById(R.id.display_gioco_seriale);
+        giocoSeriale.setText(ser);
+        
+        Log.d(TAG,"Qualcosa è successo "+name+" "+res);
 		// do something with tagFromIntent
 	}
 
