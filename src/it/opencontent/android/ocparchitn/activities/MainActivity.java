7d8e3ed10fb1e5db2c75dc10e7c8173ed84eac5d
Marco Albarelli <info@marcoalbarelli.eu>
2012-10-24 20:03:25 +0200

    Modificato l'intent filter, sia nel manifest che nell'onCreate Modificato il metodo onNewIntent

--- src/it/opencontent/android/ocparchitn/activities/MainActivity.java
+++ src/it/opencontent/android/ocparchitn/activities/MainActivity.java
@@ -13,12 +13,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
 import android.graphics.Bitmap;
+import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.MifareClassic;
 import android.nfc.tech.MifareUltralight;
 import android.nfc.tech.NfcF;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -34,23 +36,21 @@ public class MainActivity extends BaseActivity {
 	private static Bitmap mImageBitmap;
 	private Pattern p = Pattern.compile("ID:\\d+");
     private Pattern p2 = Pattern.compile("SER:[\\p{L}0-9-_ :]+");
+    
+    private NdefMessage[] msgs;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		nfca = NfcAdapter.getDefaultAdapter(this);
-		pi = PendingIntent.getActivity(this, 0, new Intent(this, getClass())
-				.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-
+		pi = PendingIntent.getActivity (this, 0, new Intent(this, getClass())
+				.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), PendingIntent.FLAG_CANCEL_CURRENT);
 		IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
 		try {
-			ndef.addDataType("*/*"); /*
-									 * Handles all MIME based dispatches. You
-									 * should specify only the ones that you
-									 * need.
-									 */
-		} catch (MalformedMimeTypeException e) {
+			ndef.addDataScheme(getString(R.string.schema_struttura));
+			ndef.addDataAuthority(getString(R.string.host_struttura), null);
+		} catch (Exception e) {
 			throw new RuntimeException("fail", e);
 		}
 		ifa = new IntentFilter[] { ndef, };
@@ -81,54 +81,26 @@ public class MainActivity extends BaseActivity {
 
 	@Override
 	public void onNewIntent(Intent intent) {
-		String res = "";
-		MifareUltralight mifare = MifareUltralight.get((Tag) intent.getParcelableExtra(NfcAdapter.EXTRA_TAG));
-		byte[] payload;
-		byte[] payload2;
-		byte[] payload3;
+		NdefMessage rawMsg = (NdefMessage) intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)[0];
+		int length = rawMsg.toByteArray().length;
+		byte[] res = new byte[length-5];
+		for(int i = 5; i <  length; i++){
+			res[i-5] =  rawMsg.toByteArray()[i];
+		}
+		String out = new String(res);
+		String[] pieces = out.split("://");
 		
-        try {
-            mifare.connect();
-            payload = mifare.readPages(4);
-            payload2 = mifare.readPages(8);
-            payload3 = mifare.readPages(12);
- 
-            //return new String(payload, Charset.forName("US-ASCII"));
-            res = new String(payload, Charset.forName("UTF-8"));
-            res += new String(payload2, Charset.forName("UTF-8"));
-            res += new String(payload3, Charset.forName("UTF-8"));
-        } catch (IOException e) {
-            Log.e(TAG, "IOException while writing MifareUltralight message...", e);
-        } finally {
-            if (mifare != null) {
-               try {
-                   mifare.close();
-               }
-               catch (IOException e) {
-                   Log.e(TAG, "Error closing tag...", e);
-               }
-            }
-        }
-        
-        
-        Matcher m = p.matcher(res);
-        Matcher m2 = p2.matcher(res);
-        String name = "";
-        String ser = "";
-        while (m.find()) { // Find each match in turn; String can't do this.
-            name = m.group(); // Access a submatch group; String can't do this.
-        }
-        while (m2.find()) { // Find each match in turn; String can't do this.
-        	ser = m2.group(); // Access a submatch group; String can't do this.
-        }
+		String[] actualValues = pieces[1].split("/");
+		String id = actualValues[1];
+			
+		String name = "Id: "+id;
+		String ser = "Originale: "+out;
         
         TextView giocoId = (TextView) findViewById(R.id.display_gioco_id);
         giocoId.setText(name);
         TextView giocoSeriale = (TextView) findViewById(R.id.display_gioco_seriale);
         giocoSeriale.setText(ser);
         
-        Log.d(TAG,"Qualcosa è successo "+name+" "+res);
-		// do something with tagFromIntent
 	}
 
 	public void takeSnapshot(View button) {
